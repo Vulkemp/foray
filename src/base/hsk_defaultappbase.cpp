261342a72f166c23fb963f82a1edff25aacffe6b
maxthi <maxthi@protonmail.com>
2022-04-07 15:36:35 +0200

    swap chain default setup using vk bootstrap

--- src/base/hsk_defaultappbase.cpp
+++ src/base/hsk_defaultappbase.cpp
@@ -116,35 +116,19 @@ namespace hsk {
 
     void DefaultAppBase::BaseInitBuildSwapchain() {
         vkb::SwapchainBuilder swapchainBuilder(mVkbDevice, mSurface);
-        
-        // Swapchain presentation modes, description from vulkan-tutorial.com
-        // VK_PRESENT_MODE_IMMEDIATE_KHR:
-        //      Images submitted by your application are transferred to the screen right away,
-        //      which may result in tearing.
-        // 
-        // VK_PRESENT_MODE_FIFO_KHR: 
-        //      The swap chain is a queue where the display takes an image from the front of the queue
-        //      when the display is refreshed and the program inserts rendered images at the back of the queue.
-        //      If the queue is full then the program has to wait. This is most similar to vertical sync as found in modern games.
-        //      The moment that the display is refreshed is known as "vertical blank".
-        // 
-        // VK_PRESENT_MODE_FIFO_RELAXED_KHR:
-        //      This mode only differs from the previous one if the application is late and the queue was empty at the last vertical blank.
-        //      Instead of waiting for the next vertical blank, the image is transferred right away when it finally arrives. This may result in visible tearing.
-        // 
-        // VK_PRESENT_MODE_MAILBOX_KHR:
-        //      This is another variation of the second mode. Instead of blocking the application when the queue is full,
-        //      the images that are already queued are simply replaced with the newer ones.
-        //      This mode can be used to render frames as fast as possible while still avoiding tearing,
-        //      resulting in fewer latency issues than standard vertical sync. This is commonly known as "triple buffering",
-        //      although the existence of three buffers alone does not necessarily mean that the framerate is unlocked.
-        
-        // use mailbox if possible, else fallback to fifo
-        swapchainBuilder.set_desired_present_mode(VK_PRESENT_MODE_MAILBOX_KHR);
-        swapchainBuilder.add_fallback_present_mode(VK_PRESENT_MODE_FIFO_KHR);
 
-        // TODO: simple query for the desired format
-        //swapchainBuilder.set_desired_format(VK_FORMAT_B8G8R8A8_SRGB);
+        // default swapchain image formats:
+        // color format: VK_FORMAT_B8G8R8A8_SRGB
+        // color space: VK_COLOR_SPACE_SRGB_NONLINEAR_KHR
+        swapchainBuilder.use_default_format_selection();
+
+        // tell vulkan the swapchain images will be used as color attachments
+        swapchainBuilder.use_default_image_usage_flags();
+
+        // use mailbox if possible, else fallback to fifo
+        swapchainBuilder.use_default_present_mode_selection();
+
+        swapchainBuilder.use_default_format_feature_flags();
 
         BeforeSwapchainBuilding(swapchainBuilder);
 
