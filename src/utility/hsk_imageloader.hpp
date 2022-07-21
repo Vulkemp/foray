3f240df6bc4ef530e82b61c1fff7da03ca418337
Joseph Heetel <joseph.heetel@protonmail.com>
2022-07-21 15:44:04 +0200

    imageloader wip

--- src/utility/hsk_imageloader.hpp
+++ src/utility/hsk_imageloader.hpp
@@ -8,34 +8,63 @@ namespace hsk {
     class ImageLoader
     {
       public:
+        /// @brief Component types
         enum class EPixelComponent
         {
+            // 16 bit float
             Fp16,
+            // 32 bit float
             Fp32,
-            Int16,
+            // 8 bit unsigned int
             UInt8,
+            // 8 bit signed int
+            Int8,
+            Int16,
+            UInt16,
+            Int32,
+            UInt32
+        };
+
+        enum class EChannel
+        {
+            Unknown = -1,
+            R       = 0,
+            G,
+            B,
+            A,
+            Y
         };
 
         class ImageInfo : public NoMoveDefaults
         {
           public:
-            bool                   Valid                   = false;
-            std::string            Utf8Path                = "";
-            std::string_view       Extension               = "";
-            std::string_view       Name                    = "";
-            EPixelComponent        PixelComponent          = EPixelComponent::Fp16;
-            uint32_t               PixelComponentSize      = 0;
-            VkExtent2D             Extent                  = VkExtent2D{};
-            bool                   IsGrayScale             = false;
-            bool                   HasAlphaChannel         = false;
-            void*                  CustomLoaderInfo        = nullptr;
+            struct ChannelConfig
+            {
+              public:
+                EChannel Channel;
+                bool     Transfer;
+            };
+
+            bool                       Valid          = false;
+            std::string                Utf8Path       = "";
+            std::string_view           Extension      = "";
+            std::string_view           Name           = "";
+            EPixelComponent            PixelComponent = EPixelComponent::Fp16;
+            std::vector<ChannelConfig> Channels;
+            VkExtent2D                 Extent                  = VkExtent2D{};
+            bool                       IsGrayScale             = false;
+            bool                       HasAlphaChannel         = false;
+            void*                      CustomLoaderInfo        = nullptr;
             std::function<void(void*)> CustomLoaderInfoDeleter = {};
 
             inline ImageInfo() {}
 
             virtual ~ImageInfo();
 
-            VkFormat sGetDefaultFormat() const;
+            inline uint32_t GetPixelComponentSize() const;
+            VkFormat        sGetDefaultFormat() const;
+            inline size_t   NaiveDataSize() const;
+            inline uint32_t TransferChannelCount() const;
         };
 
         static bool sGetImageInfo(std::string_view utf8path, ImageInfo& out);
@@ -64,4 +93,40 @@ namespace hsk {
         bool        Load_StbHdr();
         bool        Load_StbLdr();
     };
+
+    inline uint32_t ImageLoader::ImageInfo::GetPixelComponentSize() const
+    {
+        switch(PixelComponent)
+        {
+            case EPixelComponent::UInt8:
+            case EPixelComponent::Int8:
+                return 1;
+            case EPixelComponent::Int16:
+            case EPixelComponent::UInt16:
+            case EPixelComponent::Fp16:
+                return 2;
+            case EPixelComponent::Int32:
+            case EPixelComponent::UInt32:
+            case EPixelComponent::Fp32:
+                return 4;
+            default:
+                return 0;
+        }
+    }
+    inline size_t ImageLoader::ImageInfo::NaiveDataSize() const
+    {
+        return Extent.width * Extent.height * TransferChannelCount() * GetPixelComponentSize();
+    }
+    inline uint32_t ImageLoader::ImageInfo::TransferChannelCount() const
+    {
+        uint32_t result = 0;
+        for(auto& channel : Channels)
+        {
+            if(channel.Transfer)
+            {
+                result++;
+            }
+        }
+        return result;
+    }
 }  // namespace hsk
--- src/utility/hsk_imageloader_exr.cpp
+++ src/utility/hsk_imageloader_exr.cpp
@@ -21,128 +21,59 @@ namespace hsk {
         inline ~ExrLoaderCache() { FreeEXRHeader(&Header); }
     };
 
-    void lReadFp16Exr(std::vector<uint8_t>&                      out,
-                      const EXRHeader&                           header,
-                      const EXRImage&                            image,
-                      const EXRTile*                             tiles,
-                      uint32_t                                   numTiles,
-                      uint32_t                                   tileHeight,
-                      uint32_t                                   tileWidth,
-                      std::unordered_map<std::string, uint32_t>& channelMap)
+    template <typename component_t>
+    void lReadExr(std::vector<uint8_t>&                               out,
+                  const EXRHeader&                                    header,
+                  const EXRImage&                                     image,
+                  const EXRTile*                                      tiles,
+                  uint32_t                                            numTiles,
+                  uint32_t                                            tileHeight,
+                  uint32_t                                            tileWidth,
+                  std::vector<ImageLoader::ImageInfo::ChannelConfig>& channels,
+                  uint32_t                                            channelStride)
     {
-        using fp16_t           = uint16_t;
-        uint8_t* writeData     = out.data();
-        fp16_t*  writeDataFp16 = reinterpret_cast<uint16_t*>(writeData);
+        component_t* writeData = reinterpret_cast<component_t*>(out.data());
+
+        auto channelToString = std::unordered_map<ImageLoader::EChannel, const char*>({
+            {ImageLoader::EChannel::R, "R"},
+            {ImageLoader::EChannel::G, "G"},
+            {ImageLoader::EChannel::B, "B"},
+            {ImageLoader::EChannel::A, "A"},
+            {ImageLoader::EChannel::Y, "Y"},
+        });
+
 
         for(int tile_index = 0; tile_index < numTiles; tile_index++)
         {
             const EXRTile& tile = tiles[tile_index];
+            int            tw   = tile.width;
+            int            th   = tile.height;
 
-            int tw = tile.width;
-            int th = tile.height;
-
-            const fp16_t* r_channel_start = reinterpret_cast<const fp16_t*>(tile.images[channelMap["R"]]);
-            const fp16_t* g_channel_start = reinterpret_cast<const fp16_t*>(tile.images[channelMap["G"]]);
-            const fp16_t* b_channel_start = reinterpret_cast<const fp16_t*>(tile.images[channelMap["B"]]);
-            const fp16_t* a_channel_start = nullptr;
-            if(channelMap.contains("A"))
+            int channelWriteOffset = 0;
+            for(int channel_index = 0; channel_index < channels.size(); channel_index++)
             {
-                a_channel_start = reinterpret_cast<const fp16_t*>(tile.images[channelMap["A"]]);
-            }
-
-            fp16_t* first_row_w16 = writeDataFp16 + (tile.offset_y * tileHeight * image.width + tile.offset_x * tileWidth) * 4;
-
-            for(int y = 0; y < th; y++)
-            {
-                const fp16_t* r_channel = r_channel_start + y * tileWidth;
-                const fp16_t* g_channel = g_channel_start + y * tileWidth;
-                const fp16_t* b_channel = b_channel_start + y * tileWidth;
-                const fp16_t* a_channel = nullptr;
-                if(a_channel_start)
+                if(!channels[channel_index].Transfer)
                 {
-                    a_channel = a_channel_start + y * tileWidth;
+                    continue;
                 }
 
-                fp16_t* row_w = first_row_w16 + (y * image.width * 4);
+                const component_t* readStart = reinterpret_cast<const component_t*>(tile.images[channel_index]);
 
-                for(int x = 0; x < tw; x++)
+                component_t* writeStart = writeData + (tile.offset_y * tileHeight * image.width + tile.offset_x * tileWidth) * channelStride;
+
+                for(int y = 0; y < th; y++)
                 {
-                    fp16_t r = *r_channel++;
-                    fp16_t g = *g_channel++;
-                    fp16_t b = *b_channel++;
-                    fp16_t a = 0x3C00;  // float 16 representation of 1.0
-                    if(a_channel)
+                    const component_t* readRowPos = readStart + y * tw;
+
+                    component_t* writeRowPos = writeStart + (y * image.width * channelStride);
+
+                    for(int x = 0; x < tw; x++)
                     {
-                        a = *a_channel++;
+                        component_t v = *readRowPos;
+                        readRowPos++;
+                        *writeRowPos = v;
+                        writeRowPos += channelStride;
                     }
-
-                    *row_w++ = r;
-                    *row_w++ = g;
-                    *row_w++ = b;
-                    *row_w++ = a;
-                }
-            }
-        }
-    }
-
-    void lReadFp32Exr(std::vector<uint8_t>&                      out,
-                      const EXRHeader&                           header,
-                      const EXRImage&                            image,
-                      const EXRTile*                             tiles,
-                      uint32_t                                   numTiles,
-                      uint32_t                                   tileHeight,
-                      uint32_t                                   tileWidth,
-                      std::unordered_map<std::string, uint32_t>& channelMap)
-    {
-        uint8_t* writeData     = out.data();
-        fp32_t*  writeDataFp32 = reinterpret_cast<fp32_t*>(writeData);
-
-        for(int tile_index = 0; tile_index < numTiles; tile_index++)
-        {
-            const EXRTile& tile = tiles[tile_index];
-
-            int tw = tile.width;
-            int th = tile.height;
-
-            const fp32_t* r_channel_start = reinterpret_cast<const fp32_t*>(tile.images[channelMap["R"]]);
-            const fp32_t* g_channel_start = reinterpret_cast<const fp32_t*>(tile.images[channelMap["G"]]);
-            const fp32_t* b_channel_start = reinterpret_cast<const fp32_t*>(tile.images[channelMap["B"]]);
-            const fp32_t* a_channel_start = nullptr;
-            if(channelMap.contains("A"))
-            {
-                a_channel_start = reinterpret_cast<const float*>(tile.images[channelMap["A"]]);
-            }
-
-            fp32_t* first_row_w32 = writeDataFp32 + (tile.offset_y * tileHeight * image.width + tile.offset_x * tileWidth) * 4;
-
-            for(int y = 0; y < th; y++)
-            {
-                const fp32_t* r_channel = r_channel_start + y * tileWidth;
-                const fp32_t* g_channel = g_channel_start + y * tileWidth;
-                const fp32_t* b_channel = b_channel_start + y * tileWidth;
-                const fp32_t* a_channel = nullptr;
-                if(a_channel_start)
-                {
-                    a_channel = a_channel_start + y * tileWidth;
-                }
-
-                fp32_t* row_w = first_row_w32 + (y * image.width * 4);
-
-                for(int x = 0; x < tw; x++)
-                {
-                    fp32_t r = *r_channel++;
-                    fp32_t g = *g_channel++;
-                    fp32_t b = *b_channel++;
-                    fp32_t a = 1.f;
-                    if(a_channel)
-                    {
-                        a = *a_channel++;
-                    }
-
-                    *row_w++ = r;
-                    *row_w++ = g;
-                    *row_w++ = b;
-                    *row_w++ = a;
                 }
             }
         }
@@ -180,35 +111,62 @@ namespace hsk {
             return false;
         }
 
-        auto& header   = loaderCache.Header;
-        auto& channels = loaderCache.Channels;
+        auto& header = loaderCache.Header;
+        // auto& channels = loaderCache.Channels;
+
+        auto stringToChannel = std::unordered_map<std::string_view, EChannel>({
+            {"R", EChannel::R},
+            {"G", EChannel::G},
+            {"B", EChannel::B},
+            {"A", EChannel::A},
+            {"Y", EChannel::Y},
+        });
 
         // STEP #2: Process header information
-
-        // Detect use of fp16
-        bool isFp16 = false;
         // Map channel names to indices
+
+        if(header.num_channels > 0)
+        {
+            switch(header.pixel_types[0])
+            {
+                case TINYEXR_PIXELTYPE_UINT:
+                    out.PixelComponent = EPixelComponent::UInt32;
+                    break;
+                case TINYEXR_PIXELTYPE_HALF:
+                    out.PixelComponent = EPixelComponent::Fp16;
+                    break;
+                case TINYEXR_PIXELTYPE_FLOAT:
+                    out.PixelComponent = EPixelComponent::Fp32;
+                    break;
+            }
+        }
 
         for(uint32_t channelIndex = 0; channelIndex < header.num_channels; channelIndex++)
         {
-            auto& channel          = header.channels[channelIndex];
-            channels[channel.name] = channelIndex;
-            if(header.pixel_types[channelIndex] == TINYEXR_PIXELTYPE_HALF)
+            auto& channel = header.channels[channelIndex];
+            if(header.pixel_types[channelIndex] != header.pixel_types[0])
             {
-                isFp16                                       = true;
-                channels[header.channels[channelIndex].name] = channelIndex;
+                HSK_THROWFMT("Image Loader: .EXR image \"{}\" has different pixel types per channel, which is not supported", out.Name);
             }
-        }
-        out.PixelComponent     = isFp16 ? EPixelComponent::Fp16 : EPixelComponent::Fp32;
-        out.PixelComponentSize = isFp16 ? 2U : 4U;
-        out.HasAlphaChannel    = channels.contains("A");
-        out.IsGrayScale        = channels.contains("Y");
-
-        if(!out.IsGrayScale && (!channels.contains("R") || !channels.contains("G") || !channels.contains("B")))
-        {
-            // No channel types we can use
-            logger()->warn("Failed to read image file \"{}\". Missing channels for full RGB.");
-            return false;
+            auto iter = stringToChannel.find(channel.name);
+            if(iter != stringToChannel.end())
+            {
+                ImageInfo::ChannelConfig config{.Channel = iter->second, .Transfer = true};
+                out.Channels.push_back(config);
+                if(config.Channel == EChannel::A)
+                {
+                    out.HasAlphaChannel = true;
+                }
+                else if(config.Channel == EChannel::Y)
+                {
+                    out.IsGrayScale = true;
+                }
+            }
+            else
+            {
+                ImageInfo::ChannelConfig config{.Channel = EChannel::Unknown, .Transfer = false};
+                out.Channels.push_back(config);
+            }
         }
 
         out.Valid = true;
@@ -241,9 +199,8 @@ namespace hsk {
 
         // STEP #4: Move EXR lib data to custom data vector
 
-        std::vector<uint8_t> data;
         // Storing as VkFormat::VK_FORMAT_R16G16B16A16_SFLOAT, so resize to pixelcount * 4 components * x bytes per component (2 or 4)
-        data.resize(image.width * image.height * 4 * mInfo.PixelComponentSize);
+        mRawData.resize(mInfo.NaiveDataSize());
 
         EXRTile single_image_tile;
         int     num_tiles;
@@ -276,13 +233,17 @@ namespace hsk {
             exr_tiles   = image.tiles;
         }
 
-        if(mInfo.PixelComponentSize == 2)
+        switch(mInfo.PixelComponent)
         {
-            lReadFp16Exr(mRawData, header, image, exr_tiles, num_tiles, tile_height, tile_width, channelMap);
-        }
-        else
-        {
-            lReadFp32Exr(mRawData, header, image, exr_tiles, num_tiles, tile_height, tile_width, channelMap);
+            case EPixelComponent::Fp16:
+                lReadExr<uint16_t>(mRawData, header, image, exr_tiles, num_tiles, tile_height, tile_width, mInfo.Channels, mInfo.TransferChannelCount());
+                break;
+            case EPixelComponent::Fp32:
+                lReadExr<fp32_t>(mRawData, header, image, exr_tiles, num_tiles, tile_height, tile_width, mInfo.Channels, mInfo.TransferChannelCount());
+                break;
+            case EPixelComponent::UInt32:
+                lReadExr<uint32_t>(mRawData, header, image, exr_tiles, num_tiles, tile_height, tile_width, mInfo.Channels, mInfo.TransferChannelCount());
+                break;
         }
 
         return true;
