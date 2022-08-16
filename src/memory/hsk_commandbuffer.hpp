36df18109f7ca795ee3b3e6b81648c37e1a6edd7
HskLaptop <hsk_xmg_ultra@protonmail.com>
2022-08-16 15:40:27 +0200

    bit of cleanup

--- src/memory/hsk_commandbuffer.hpp
+++ src/memory/hsk_commandbuffer.hpp
@@ -5,19 +5,28 @@
 
 namespace hsk {
 
+    /// @brief VkCommandBuffer wrapper
     class CommandBuffer : public DeviceResourceBase
     {
       public:
         CommandBuffer() = default;
         inline virtual ~CommandBuffer() { Cleanup(); }
 
+        /// @brief Create based on the contexts device, command pool and queue.
         VkCommandBuffer Create(const VkContext* context, VkCommandBufferLevel cmdBufferLvl = VK_COMMAND_BUFFER_LEVEL_PRIMARY, bool begin = false);
+        /// @brief Create based on custom vulkan context
         VkCommandBuffer Create(VkDevice device, VkCommandPool cmdPool, Queue queue, VkCommandBufferLevel cmdBufferLvl = VK_COMMAND_BUFFER_LEVEL_PRIMARY, bool begin = false);
-        void            Begin();
-        void            Submit(bool fireAndForget = false);
-        void            FlushAndReset();
-        void            WaitForCompletion();
+        /// @brief vkBeginCommandBuffer();
+        void Begin();
+        /// @brief vkSubmitCommandBuffer();
+        /// @remark If fireAndForget is not set, will await signal to the included fence to assure the command buffer has run to completion
+        void Submit(bool fireAndForget = false);
+        /// @brief Submit, await completion, then reset
+        void FlushAndReset();
+        /// @brief Waits for the included fence to signal the command buffer has finished executing
+        void WaitForCompletion();
 
+        /// @brief Destroys the associated resources
         virtual void Cleanup() override;
         virtual bool Exists() const override { return mCommandBuffer; }
 
@@ -31,10 +40,10 @@ namespace hsk {
             VkDevice      Device;
             VkCommandPool Pool;
             Queue         ExecutingQueue;
-        } mContext;
+        } mContext = {};
 
-        VkCommandBuffer  mCommandBuffer{};
-        VkFence          mFence{};
+        VkCommandBuffer mCommandBuffer{};
+        VkFence         mFence{};
     };
 
 }  // namespace hsk
--- src/memory/hsk_vmaHelpers.cpp
+++
@@ -1,7 +0,0 @@
-#include "hsk_vmaHelpers.hpp"
-#include "../base/hsk_logger.hpp"
-#include "../hsk_vkHelpers.hpp"
-#include "hsk_managedbuffer.hpp"
-
-namespace hsk {
-}  // namespace hsk
