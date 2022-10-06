54a8f2fb4eec779c1eda084c8d63d91a617bbe71
Joseph Heetel <joseph.heetel@protonmail.com>
2022-10-06 15:32:55 +0200

    Add externally used timeline semaphore for denoiser sync

--- src/stages/hsk_denoiserstage.hpp
+++ src/stages/hsk_denoiserstage.hpp
@@ -2,20 +2,42 @@
 #include "hsk_renderstage.hpp"
 
 namespace hsk {
+    class DenoiserSynchronisationSemaphore : public DeviceResourceBase
+    {
+      public:
+        void                Create(const VkContext* context);
+        inline virtual bool Exists() const override { return !!mSemaphore; }
+        virtual void        Destroy() override;
+
+        HSK_PROPERTY_CGET(Semaphore)
+        HSK_PROPERTY_CGET(Handle)
+        HSK_PROPERTY_ALL(Value)
+
+      protected:
+        const VkContext* mContext   = nullptr;
+        VkSemaphore      mSemaphore = nullptr;
+        uint64_t         mValue     = 0;
+#ifdef WIN32
+        HANDLE mHandle = INVALID_HANDLE_VALUE;
+#else
+        int mHandle = -1;
+#endif
+    };
+
     class DenoiserConfig
     {
       public:
-        ManagedImage*              PrimaryInput = nullptr;
-        ManagedImage*              AlbedoInput  = nullptr;
-        ManagedImage*              NormalInput  = nullptr;
-        std::vector<ManagedImage*> AuxiliaryInputs;
-        ManagedImage*              PrimaryOutput = nullptr;
-        void*                      AuxiliaryData = nullptr;
-        VkSemaphore                Semaphore     = nullptr;
+        ManagedImage*                     PrimaryInput = nullptr;
+        ManagedImage*                     AlbedoInput  = nullptr;
+        ManagedImage*                     NormalInput  = nullptr;
+        std::vector<ManagedImage*>        AuxiliaryInputs;
+        ManagedImage*                     PrimaryOutput = nullptr;
+        void*                             AuxiliaryData = nullptr;
+        DenoiserSynchronisationSemaphore* Semaphore     = nullptr;
 
         inline DenoiserConfig() {}
-        inline DenoiserConfig(ManagedImage* primaryIn, ManagedImage* albedoIn, ManagedImage* normalIn, ManagedImage* primaryOut, VkSemaphore semaphore)
-            : PrimaryInput(primaryIn), AlbedoInput(albedoIn), NormalInput(normalIn), PrimaryOutput(primaryOut), Semaphore(semaphore)
+        inline DenoiserConfig(ManagedImage* primaryIn, ManagedImage* albedoIn, ManagedImage* normalIn, ManagedImage* primaryOut, DenoiserSynchronisationSemaphore& semaphore)
+            : PrimaryInput(primaryIn), AlbedoInput(albedoIn), NormalInput(normalIn), PrimaryOutput(primaryOut), Semaphore(&semaphore)
         {
         }
     };
