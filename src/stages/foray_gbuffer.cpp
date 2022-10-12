9e538ec0549c47d5e65590482dbb8315b1c1b38a
maxthi <maxthi@protonmail.com>
2022-10-12 17:23:46 +0200

    Allow sampling of gbuffer images

--- src/stages/foray_gbuffer.cpp
+++ src/stages/foray_gbuffer.cpp
@@ -115,7 +115,7 @@ namespace foray::stages {
         static const VkFormat geometryFormat = VK_FORMAT_R16G16B16A16_SFLOAT;
 
         static const VkImageUsageFlags imageUsageFlags =
-            VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+            VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
 
         VkExtent3D               extent                = {mContext->Swapchain.extent.width, mContext->Swapchain.extent.height, 1};
         VmaMemoryUsage           memoryUsage           = VMA_MEMORY_USAGE_AUTO_PREFER_DEVICE;
