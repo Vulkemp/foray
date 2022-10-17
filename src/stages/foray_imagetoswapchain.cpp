f6fd50365b4e4275318b34da3dca0c17d0ad157e
Joseph Heetel <joseph.heetel@protonmail.com>
2022-10-17 22:17:26 +0200

    Fix image 2 swapchain sync validation error

--- src/stages/foray_imagetoswapchain.cpp
+++ src/stages/foray_imagetoswapchain.cpp
@@ -26,46 +26,56 @@ namespace foray::stages {
 
     void ImageToSwapchainStage::RecordFrame(VkCommandBuffer cmdBuffer, base::FrameRenderInfo& renderInfo)
     {
-        VkImageSubresourceRange range{};
-        range.aspectMask     = VkImageAspectFlagBits::VK_IMAGE_ASPECT_COLOR_BIT;
-        range.baseMipLevel   = 0;
-        range.levelCount     = 1;
-        range.baseArrayLayer = 0;
-        range.layerCount     = 1;
+        uint32_t swapChainImageIndex = renderInfo.GetInFlightFrame()->GetSwapchainImageIndex();
 
-        VkImageMemoryBarrier barrier{};
-        barrier.sType            = VkStructureType::VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
-        barrier.subresourceRange = range;
+        VkImage swapImage = mContext->ContextSwapchain.SwapchainImages[swapChainImageIndex].Image;
 
-        uint32_t        swapChainImageIndex = renderInfo.GetInFlightFrame()->GetSwapchainImageIndex();
+        VkImageMemoryBarrier2 swapImgMemBarrier{
+            .sType               = VkStructureType::VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2,
+            .srcStageMask        = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
+            .srcAccessMask       = VK_ACCESS_2_MEMORY_READ_BIT | VK_ACCESS_2_MEMORY_WRITE_BIT,
+            .dstStageMask        = VK_PIPELINE_STAGE_2_BLIT_BIT,
+            .dstAccessMask       = VK_ACCESS_2_TRANSFER_WRITE_BIT,
+            .oldLayout           = VkImageLayout::VK_IMAGE_LAYOUT_UNDEFINED,
+            .newLayout           = VkImageLayout::VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+            .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .image               = swapImage,
+            .subresourceRange =
+                VkImageSubresourceRange{
+                    .aspectMask = VkImageAspectFlagBits::VK_IMAGE_ASPECT_COLOR_BIT,
+                    .levelCount = 1,
+                    .layerCount = 1,
+                },
+        };
 
-        // Barrier: Grab swapchain image, change it into transfer dst layout
-        barrier.srcAccessMask       = 0;                                               // since no memory operations happen before layout transition, we don't care of a srcMask
-        barrier.dstAccessMask       = VkAccessFlagBits::VK_ACCESS_TRANSFER_WRITE_BIT;  // we block memory transfer to this swapchain image before layout transition
-        barrier.oldLayout           = VkImageLayout::VK_IMAGE_LAYOUT_UNDEFINED;
-        barrier.newLayout           = VkImageLayout::VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
-        barrier.srcQueueFamilyIndex = mContext->QueueGraphics;
-        barrier.dstQueueFamilyIndex = mContext->QueueGraphics;
-        barrier.image               = mContext->ContextSwapchain.SwapchainImages[swapChainImageIndex].Image;
+        VkImageMemoryBarrier2 srcImgMemBarrier{
+            .sType               = VkStructureType::VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2,
+            .srcStageMask        = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
+            .srcAccessMask       = VK_ACCESS_2_MEMORY_WRITE_BIT,
+            .dstStageMask        = VK_PIPELINE_STAGE_2_BLIT_BIT,
+            .dstAccessMask       = VK_ACCESS_2_TRANSFER_READ_BIT,
+            .oldLayout           = VkImageLayout::VK_IMAGE_LAYOUT_UNDEFINED,
+            .newLayout           = VkImageLayout::VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+            .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .image               = mSourceImage->GetImage(),
+            .subresourceRange =
+                VkImageSubresourceRange{
+                    .aspectMask = VkImageAspectFlagBits::VK_IMAGE_ASPECT_COLOR_BIT,
+                    .levelCount = 1,
+                    .layerCount = 1,
+                },
+        };
 
-        // srcStage: since we aquire a swapchain image, we don't need to wait for any commands to be processed, so we use top of pipe bit = wait for nothing
-        // dstStage: we execute a transfer command later, so we block transfer until the layout has been transfered.
-        vkCmdPipelineBarrier(cmdBuffer, VkPipelineStageFlagBits::VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VkPipelineStageFlagBits::VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, nullptr, 0,
-                             nullptr, 1, &barrier);
+        std::vector<VkImageMemoryBarrier2> barriers({swapImgMemBarrier, srcImgMemBarrier});
 
-        core::ManagedImage::LayoutTransitionInfo layoutTransitionInfo;
-        layoutTransitionInfo.CommandBuffer        = cmdBuffer;
-        layoutTransitionInfo.BarrierSrcAccessMask = VkAccessFlagBits::VK_ACCESS_MEMORY_WRITE_BIT;
-        layoutTransitionInfo.BarrierDstAccessMask = VkAccessFlagBits::VK_ACCESS_TRANSFER_READ_BIT;
-        layoutTransitionInfo.OldImageLayout       = VkImageLayout::VK_IMAGE_LAYOUT_UNDEFINED;
-        layoutTransitionInfo.NewImageLayout       = VkImageLayout::VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
-        layoutTransitionInfo.SrcQueueFamilyIndex  = mContext->QueueGraphics;
-        layoutTransitionInfo.DstQueueFamilyIndex  = mContext->QueueGraphics;
-        layoutTransitionInfo.SubresourceRange     = range;
-        layoutTransitionInfo.SrcStage             = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;  // our source image was a color attachment in a previous pass
-        layoutTransitionInfo.DstStage             = VK_PIPELINE_STAGE_TRANSFER_BIT;                 // we block transfer before layout transition is done
-        mSourceImage->TransitionLayout(layoutTransitionInfo);
+        VkDependencyInfo depInfo{.sType                   = VkStructureType::VK_STRUCTURE_TYPE_DEPENDENCY_INFO,
+                                 .dependencyFlags         = VkDependencyFlagBits::VK_DEPENDENCY_BY_REGION_BIT,
+                                 .imageMemoryBarrierCount = (uint32_t)barriers.size(),
+                                 .pImageMemoryBarriers    = barriers.data()};
 
+        vkCmdPipelineBarrier2(cmdBuffer, &depInfo);
 
         VkImageSubresourceLayers layers = {};
         layers.aspectMask               = VkImageAspectFlagBits::VK_IMAGE_ASPECT_COLOR_BIT;
@@ -75,46 +85,14 @@ namespace foray::stages {
 
         VkImageBlit blitRegion    = {};
         blitRegion.srcSubresource = layers;
-        blitRegion.srcOffsets[0]  = VkOffset3D{.x = (int32_t)mContext->Swapchain.extent.width, .y = (int32_t)mContext->Swapchain.extent.height, .z = 0};
-        blitRegion.srcOffsets[1]  = {.z = 1};
+        blitRegion.srcOffsets[0]  = {};
+        blitRegion.srcOffsets[1]  = VkOffset3D{.x = (int32_t)mContext->Swapchain.extent.width, .y = (int32_t)mContext->Swapchain.extent.height, .z = 1};
         blitRegion.dstSubresource = layers;
-        blitRegion.dstOffsets[0]  = VkOffset3D{.x = (int32_t)mContext->Swapchain.extent.width, .y = (int32_t)mContext->Swapchain.extent.height, .z = 0};
-        blitRegion.dstOffsets[1]  = {.z = 1};
+        blitRegion.dstOffsets[0]  = {};
+        blitRegion.dstOffsets[1]  = VkOffset3D{.x = (int32_t)mContext->Swapchain.extent.width, .y = (int32_t)mContext->Swapchain.extent.height, .z = 1};
 
-        vkCmdBlitImage(cmdBuffer, mSourceImage->GetImage(), VkImageLayout::VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
-                       mContext->ContextSwapchain.SwapchainImages[swapChainImageIndex].Image, VkImageLayout::VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &blitRegion,
-                       VkFilter::VK_FILTER_NEAREST);
-
-
-        // Barrier: Change swapchain image to present layout, transfer it back to present queue
-        barrier.srcAccessMask       = VkAccessFlagBits::VK_ACCESS_TRANSFER_WRITE_BIT;  // we wait for the memory transfer to happen
-        barrier.dstAccessMask       = 0;                                               // we don't intend to do anything anymore with the resource so no commands need to be blocked
-        barrier.oldLayout           = VkImageLayout::VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
-        barrier.newLayout           = VkImageLayout::VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
-        barrier.srcQueueFamilyIndex = mContext->QueueGraphics;
-        barrier.dstQueueFamilyIndex = mContext->PresentQueue;
-
-        vkCmdPipelineBarrier(cmdBuffer,
-                             VkPipelineStageFlagBits::VK_PIPELINE_STAGE_TRANSFER_BIT,     // wait for transfer
-                             VkPipelineStageFlagBits::VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,  // block nothing
-                             0, 0, nullptr, 0, nullptr, 1, &barrier);
-
-        if(!!mPostCopy.AccessFlags && !!mPostCopy.ImageLayout)
-        {
-            // Return old image back
-            core::ManagedImage::LayoutTransitionInfo layoutTransitionInfo;
-            layoutTransitionInfo.CommandBuffer        = cmdBuffer;
-            layoutTransitionInfo.BarrierSrcAccessMask = VkAccessFlagBits::VK_ACCESS_TRANSFER_WRITE_BIT;  // flush color attachment writes
-            layoutTransitionInfo.BarrierDstAccessMask = mPostCopy.AccessFlags;                           // block transfer writes before layout transition is over
-            layoutTransitionInfo.OldImageLayout       = VkImageLayout::VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
-            layoutTransitionInfo.NewImageLayout       = mPostCopy.ImageLayout;
-            layoutTransitionInfo.SrcQueueFamilyIndex  = mContext->QueueGraphics;
-            layoutTransitionInfo.DstQueueFamilyIndex  = mPostCopy.QueueFamilyIndex;
-            layoutTransitionInfo.SubresourceRange     = range;
-            layoutTransitionInfo.SrcStage             = VK_PIPELINE_STAGE_TRANSFER_BIT;      // our source image was a color attachment in a previous pass
-            layoutTransitionInfo.DstStage             = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;  // we block transfer before layout transition is done
-            mSourceImage->TransitionLayout(layoutTransitionInfo);
-        }
+        vkCmdBlitImage(cmdBuffer, mSourceImage->GetImage(), VkImageLayout::VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, swapImage, VkImageLayout::VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1,
+                       &blitRegion, VkFilter::VK_FILTER_NEAREST);
     }
 
-}  // namespace foray
+}  // namespace foray::stages
