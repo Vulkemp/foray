fb2f81c109c36715b946207cdf88b5f18a7de030
Joseph-Heetel <joseph.heetel@protonmail.com>
2022-11-08 12:55:18 +0100

    In code documentation

--- src/foray_basics.hpp
+++ src/foray_basics.hpp
@@ -12,6 +12,7 @@ namespace foray {
     using fp32_t = float;
     using fp64_t = double;
 
+    /// @brief Simple types for supressing automatic definition of duplicating move constructors & operator
     class NoMoveDefaults
     {
       public:
@@ -21,6 +22,7 @@ namespace foray {
         NoMoveDefaults& operator=(const NoMoveDefaults& other) = delete;
     };
 
+    /// @brief Simple type for forcing a type to be polymorphic
     class Polymorphic
     {
       protected:
@@ -28,8 +30,10 @@ namespace foray {
     };
 
 #if defined(FORAY_INFLIGHTFRAMECOUNT_OVERRIDE)
+    /// @brief Inflight frame count is the amount of frames 'in flight' aka frames recorded on the host before waiting for the device to finish rendering a previous frame.
     inline constexpr uint32_t INFLIGHT_FRAME_COUNT = FORAY_INFLIGHTFRAMECOUNT_OVERRIDE;
 #else
+    /// @brief Inflight frame count is the amount of frames 'in flight' aka frames recorded on the host before waiting for the device to finish rendering a previous frame.
     inline constexpr uint32_t INFLIGHT_FRAME_COUNT = 2;
 #endif
 }  // namespace foray
@@ -62,22 +66,25 @@ namespace foray {
     FORAY_PROPERTY_CGET(member)
 
 #ifdef FORAY_ALTERNATIVE_PROPERTY_MACROS
-
+/// @brief Return value
 #define FORAY_GETTER_V(member)                                                                                                                                                     \
     inline auto Get##member() const                                                                                                                                                \
     {                                                                                                                                                                              \
         return m##member;                                                                                                                                                          \
     }
+/// @brief Return mutable reference
 #define FORAY_GETTER_MR(member)                                                                                                                                                    \
     inline auto& Get##member()                                                                                                                                                     \
     {                                                                                                                                                                              \
         return m##member;                                                                                                                                                          \
     }
+/// @brief Return constant reference
 #define FORAY_GETTER_CR(member)                                                                                                                                                    \
     inline const auto& Get##member() const                                                                                                                                         \
     {                                                                                                                                                                              \
         return m##member;                                                                                                                                                          \
     }
+/// @brief set by passing a constant reference
 #define FORAY_SETTER_R(member)                                                                                                                                                     \
     template <typename TIn>                                                                                                                                                        \
     inline auto& Set##member(const TIn& value)                                                                                                                                     \
@@ -85,6 +92,7 @@ namespace foray {
         m##member = value;                                                                                                                                                         \
         return *this;                                                                                                                                                              \
     }
+/// @brief set by passing a value
 #define FORAY_SETTER_V(member)                                                                                                                                                     \
     template <typename TIn>                                                                                                                                                        \
     inline auto& Set##member(TIn value)                                                                                                                                            \
@@ -93,10 +101,12 @@ namespace foray {
         return *this;                                                                                                                                                              \
     }
 
+/// @brief Getter+Setter shorthand for value types
 #define FORAY_PROPERTY_V(member)\
     FORAY_GETTER_V(member)\
     FORAY_SETTER_V(member)
 
+/// @brief Getter+Setter shorthand for reference types
 #define FORAY_PROPERTY_R(member)\
     FORAY_GETTER_MR(member)\
     FORAY_GETTER_CR(member)\
